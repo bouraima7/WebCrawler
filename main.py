38c772e8dd1a7f7c37c44f2465e8188cf740d0b5
boura <bouraima799@gmail.com>
2023-05-08 17:54:54 -0400

    updated changes

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
+from selenium import webdriver
 
-# Define the URL to scrape
-url = "https://nba.com"
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.nba.com/")
+driver.close()
 
-# Define the words to search for
-words_to_search = ["Embiid", "James", "Curry"]
-
-# Fetch the HTML content of the URL
-response = requests.get(url)
-html_content = response.content
-
-# Parse the HTML content with BeautifulSoup
-soup = BeautifulSoup(html_content, "html.parser")
-
-# Find all the text on the page
-all_text = soup.get_text()
-
-# Count the occurrences of each word
-word_counts = {}
-for word in words_to_search:
-    count = all_text.count(word)
-    word_counts[word] = count
-
-# Print the results
-for word, count in word_counts.items():
-    print(f"{word}: {count} occurrences")
+# # Define the URL to scrape
+# url = "https://nba.com"
+#
+# # Define the words to search for
+# words_to_search = ["Embiid", "James", "Curry"]
+#
+# # Fetch the HTML content of the URL
+# response = requests.get(url)
+# html_content = response.content
+#
+# # Parse the HTML content with BeautifulSoup
+# soup = BeautifulSoup(html_content, "html.parser")
+#
+# # Find all the text on the page
+# all_text = soup.get_text()
+#
+# # Count the occurrences of each word
+# word_counts = {}
+# for word in words_to_search:
+#     count = all_text.count(word)
+#     word_counts[word] = count
+#
+# # Print the results
+# for word, count in word_counts.items():
+#     print(f"{word}: {count} occurrences")
